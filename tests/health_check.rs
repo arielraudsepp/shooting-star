404a06d2b9b674b9e1ff95f2b1dddfae27ad73de
Ariel Raudsepp <ariel@Disthene.localdomain>
2021-10-09 21:01:19 -0700

    created test folder

---
+++ tests/health_check.rs
@@ -0,0 +1,22 @@
+//! tests/health_check.rs
+
+use shooting_star::run;
+
+#[actix_rt::test]
+ async fn health_check_works() {
+     spawn_app().await.expect("Failed to spawn app");
+     let client = reqwest::Client::new();
+
+     let response = client
+         .get("http://127.0.0.1:8080/health_check")
+         .send()
+         .await
+         .expect("Failed to execute request");
+
+     assert!(response.status().is_success());
+     assert_eq!(Some(0), response.content_length());
+ }
+
+async fn spawn_app() -> std::io::Result<()> {
+    todo!()
+}
