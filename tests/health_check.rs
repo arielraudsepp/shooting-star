d2b27e41adf35618f54fcbb6abff021ba9d67415
Ariel Raudsepp <ariel@Disthene.localdomain>
2021-10-11 12:13:43 -0700

    ran cargo fmt and clippy

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,19 +1,19 @@
 //! tests/health_check.rs
 
 #[actix_rt::test]
- async fn health_check_works() {
-     spawn_app();
-     let client = reqwest::Client::new();
+async fn health_check_works() {
+    spawn_app();
+    let client = reqwest::Client::new();
 
-     let response = client
-         .get("http://127.0.0.1:8080/health_check")
-         .send()
-         .await
-         .expect("Failed to execute request");
+    let response = client
+        .get("http://127.0.0.1:8080/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request");
 
-     assert!(response.status().is_success());
-     assert_eq!(Some(0), response.content_length());
- }
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
 
 fn spawn_app() {
     let server = shooting_star::run().expect("Failed to bind address");
