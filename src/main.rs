89746cf414a2c0f1ca1f83a6e5ec89eb04a51c2b
Ariel Raudsepp <ariel@Disthene.localdomain>
2021-10-16 19:22:06 -0700

    added configuration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 //! main.rs
-
-use shooting_star::run;
+use shooting_star::{configuration::get_configuration, run};
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
