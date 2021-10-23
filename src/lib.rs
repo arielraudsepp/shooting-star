eb74dde7fa9b1534028f432c0e10b8f1c1a1c101
Ariel Raudsepp <ariel@Disthene.local>
2021-10-23 13:59:54 -0700

    added record return in POST

--- src/lib.rs
+++ src/lib.rs
@@ -28,30 +28,31 @@ struct FormData {
 )]
 async fn enter_data(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     match insert_entry(&pool, &form).await {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(record) => HttpResponse::Ok().body(record),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
-#[tracing::instrument(name = "Saving data in the databse", skip(form, pool))]
-async fn insert_entry(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
-    sqlx::query!(
+#[tracing::instrument(name = "Saving data in the database", skip(form, pool))]
+async fn insert_entry(pool: &PgPool, form: &FormData) -> Result<String, sqlx::Error> {
+    let query = sqlx::query!(
         r#"
     INSERT INTO skills_tracker (id, skill_name, completed, entered_at)
-    VALUES ($1, $2, $3, $4)
-        "#,
+    VALUES ($1, $2, $3, $4) RETURNING id, skill_name, completed, entered_at
+"#,
         Uuid::new_v4(),
         form.skill_name,
         form.completed,
         Utc::now()
     )
-    .execute(pool)
+    .fetch_one(pool)
     .await
     .map_err(|e| {
         tracing::error!("failed to execute query: {:?}", e);
         e
     })?;
-    Ok(())
+
+    Ok(format!("{:?}", query))
 }
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
