230933aa677dbb1cd4f35b1d6dd7daebaac9c052
Ariel Raudsepp <ariel@Disthene.local>
2021-10-23 12:09:48 -0700

    created POST/enter_data

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,58 @@ use actix_web::web::Data;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 use sqlx::PgPool;
+use uuid::Uuid;
+use chrono::Utc;
+
+
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    skill_name: String,
+    completed: bool,
+}
+
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        data_skill_name = %form.skill_name,
+        data_completed = %form.completed
+    )
+)]
+async fn enter_data(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_entry(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish()
+    }
+}
+
+#[tracing::instrument(
+    name = "Saving data in the databse",
+    skip(form, pool)
+)]
+async fn insert_entry(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+    INSERT INTO skills_tracker (id, skill_name, completed, entered_at)
+    VALUES ($1, $2, $3, $4)
+        "#,
+    Uuid::new_v4(),
+    form.skill_name,
+    form.completed,
+    Utc::now()
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
@@ -15,6 +64,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/enter_data",web::post().to(enter_data))
             .app_data(db_pool.clone())
     })
     .listen(listener)?
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,16 +4,16 @@
 use std::net::TcpListener;
 use shooting_star::run;
 use shooting_star::configuration::{get_configuration, DatabaseSettings};
-use sqlx::PgPool;
+use sqlx::{PgPool, Connection, PgConnection, Executor};
 use uuid::Uuid;
 
 #[actix_rt::test]
 async fn health_check_works() {
-    spawn_app();
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8080/health_check")
+        .get(&format!("{}/health_check", &app.address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -22,21 +22,44 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
 async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap.port();
+    let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database)
-        .await
-        .expect("Failed to connect to Postgres");
-    let server = run(listener, connection_pool)
-        .expect("Failed to bind address");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    let connection_pool = configure_database(&configuration.database).await;
+    let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     TestApp {
         address,
         db_pool: connection_pool
     }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
+        .await
+        .expect("Failed to create database.");
+
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
 }
