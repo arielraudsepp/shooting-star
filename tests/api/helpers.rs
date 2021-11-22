900e1cdce75e6a3836378505941b626b9782d9f9
Ariel Raudsepp <ariel@Disthene.local>
2021-11-21 23:35:52 -0800

    Refactor how test database is made.
    In the old system, it would create a totally new database each time a
    test was run. This is nice because it ensure that the database is
    always empty each time a test is run. This make sure no conflicts
    happen (or duplicate data is created). And because we're running is a
    dockr container we, as the maintainers of the databaser, won't really
    notice all these superfluous database being created because when docker
    or our machine restarts they all get washed away.
    
    This has the downside in that we can't query our test database because
    it has a random name. So now we have chosen a static name, test_skills,
    and always create and recreate that database.
    
    There is still some work to be done in that we need to refactor our
    configuration file to contain the idea of dev and test
    configurations. The configuration file might look something like:
    
    ```
    dev:
      application_port: 8000
      database:
        host: "localhost"
        port: 5432
        username: "postgres"
        password: "password"
        database_name: "skills"
    test:
      application_port: 8000
      database:
        host: "localhost"
        port: 5432
        username: "postgres"
        password: "password"
        database_name: "test_skills"
    ```

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -17,7 +17,11 @@ pub async fn spawn_app() -> TestApp {
     let address = format!("http://127.0.0.1:{}", port);
 
     let mut configuration = get_configuration().expect("Failed to read configuration.");
-    configuration.database.database_name = Uuid::new_v4().to_string();
+    // this is the old way of naming your database
+    // configuration.database.database_name = Uuid::new_v4().to_string();
+
+    // This is the new way, it shoul be stored in your application configuration
+    configuration.database.database_name = "test_skills".to_string();
     let connection_pool = configure_database(&configuration.database).await;
     let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
@@ -27,15 +31,29 @@ pub async fn spawn_app() -> TestApp {
     }
 }
 
+pub async fn delete_database(connection: &mut PgConnection, name: &str) -> Result<(), sqlx::Error>{
+    connection
+        .execute(&*format!(r#"DROP DATABASE IF EXISTS "{}";"#, name))
+        .await?;
+    Ok(())
+}
+
+pub async fn create_database(connection: &mut PgConnection, name: &str) -> Result<() , sqlx::Error>{
+    connection
+        .execute(&*format!(r#"CREATE DATABASE "{}";"#, name))
+        .await?;
+
+    Ok(())
+}
+
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     let mut connection = PgConnection::connect(&config.connection_string_without_db())
         .await
         .expect("Failed to connect to Postgres");
-    connection
-        .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
-        .await
-        .expect("Failed to create database.");
-
+    // Should be config.datbase.name, or really config.test.database.name
+    // but as a shortcut we are hardcoding it.
+    delete_database(&mut connection, &config.database_name).await;
+    create_database(&mut connection, &config.database_name).await;
     let connection_pool = PgPool::connect(&config.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
