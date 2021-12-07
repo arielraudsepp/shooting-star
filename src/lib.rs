a6542545bb092861d17e07eb0cbf9681f01ca769
Ariel Raudsepp <ariel@Disthene.local>
2021-12-06 20:25:07 -0800

    Add CORS

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod configuration;
 
 use actix_web::dev::Server;
 use actix_web::web::Data;
+use actix_cors::Cors;
 use actix_web::{App, HttpResponse, HttpServer, web};
 use chrono::Utc;
 use sqlx::PgPool;
@@ -86,7 +87,9 @@ async fn get_entry(pool: &PgPool, id: Uuid) -> Result<Skill, sqlx::Error> {
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
+        let cors = Cors::default().allow_any_header().allow_any_method().allow_any_origin().max_age(3600);
         App::new()
+            .wrap(cors)
             .route("/health_check", web::get().to(health_check))
             .route("/enter_data", web::post().to(enter_data))
             .route("/get_data/{id}", web::get().to(get_data))
