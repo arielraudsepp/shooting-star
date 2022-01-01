a9329253ae83b9dc076eb6d3921c84731b45e2c8
Ariel Raudsepp <ariel@Disthene.local>
2021-12-31 18:35:57 -0800

    seed skills table

---
+++ src/bin/run_seeds.rs
@@ -0,0 +1,23 @@
+use shooting_star::configuration::get_configuration;
+use sqlx::postgres::PgPoolOptions;
+use std::fs;
+
+#[tokio::main]
+async fn main() {
+    let file_name = "seeds/seed_skills.sql";
+    let seed =
+        fs::read_to_string(&file_name).expect(&format!("Unable to up read from {}", &file_name));
+
+    let config = get_configuration().expect("Unable to read settings file");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(1)
+        .connect(&config.database.connection_string())
+        .await
+        .expect("Unable to connect to postgres");
+
+    sqlx::query(&seed)
+        .fetch_all(&pool)
+        .await
+        .expect("Unable to generate seed data");
+}
